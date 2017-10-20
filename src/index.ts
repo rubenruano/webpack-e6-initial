380fe965ceecb591958ce1edaf8e2570ad7ef301
Ruben D Ruano <rubenruano@gmail.com>
2017-10-20 01:39:36 -0300

    Adding support for Typescript

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello world! (from webpack-dev-server) with Typescript");
+
+const myname:string = "Ruben";
+
+console.log(`My name is ${myname}`);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
+const path = require('path');
+
 module.exports = {
-    entry: __dirname + '/app/index.js',
-    output: {
-        path: __dirname + '/build',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devServer: {
-        port: 3000,
-        contentBase: './build',
-        inline: true
-    }
-}
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }      
+    ]
+  },
+  resolve: {
+    extensions: [ ".tsx", ".ts", ".js" ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  devServer: {
+	  port: 3000,
+	  contentBase: './build',
+	  inline: true
+  }
+};
